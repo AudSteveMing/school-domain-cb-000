f78afab5bcd16504b6bc767a6f12d94b68fc7306
Steve Cordova <cordovas@bc.edu>
2020-07-31 03:49:19 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+class School
+  attr_accessor :name, :roster
+
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+
+  # def roster
+  #   @roster
+  # end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+  def add_student(name, grade)
+    roster[grade] ||= []
+    roster[grade] << name
+  end
+
+  def sort
+    roster[grade].map do |key, value|
+      key.sort
+  end
+end
+end
+
+# school.sort
+# # => {9 => ["AC Slater", "Zach Morris"], 10 => ["Aardvark", "Kelly Kapowski"], 11 => ["Screech", "Xavier"]}
